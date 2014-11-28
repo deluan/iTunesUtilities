e9eae300c2b7d9c0446d0239fffe3c2b49a49950
Deluan <deluan@thoughtworks.com>
2014-11-27 21:18:59 -0500

    Extracted common PropertyTagHandler into a base class

--- src/main/java/com/worldsworstsoftware/itunes/parser/LibraryPropertyTagHandler.java
+++ src/main/java/com/worldsworstsoftware/itunes/parser/LibraryPropertyTagHandler.java
@@ -1,36 +1,16 @@
 package com.worldsworstsoftware.itunes.parser;
 
 import com.worldsworstsoftware.itunes.ItunesLibrary;
-import com.worldsworstsoftware.xmltagparser.Tag;
-import net.vidageek.mirror.dsl.Mirror;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
-class LibraryPropertyTagHandler implements PropertyTagHandler {
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    protected static final String NO_PROPERTY = "NO PROPERTY";
-
-
-    protected ItunesLibrary library = null;
-    protected String currentProperty = NO_PROPERTY;
-
-    protected Map<String, String> propertyMap = new HashMap<String, String>();
-    protected Map<String, Class> propertyTypeMap = new HashMap<String, Class>();
-
-    protected DataTypeParser typeParser = new DataTypeParser();
+class LibraryPropertyTagHandler extends BasePropertyTagHandler {
 
     public LibraryPropertyTagHandler(ItunesLibrary library) {
-        this.library = library;
-
-        initializePropertyMap();
+        super(library);
     }
 
-    private void initializePropertyMap() {
+    protected void initializePropertyMap() {
         addPropertyToPropertyMap(LibraryProperty.MAJOR_VERSION, "majorVersion", Integer.class);
         addPropertyToPropertyMap(LibraryProperty.MINOR_VERSION, "majorVersion", Integer.class);
         addPropertyToPropertyMap(LibraryProperty.DATE, "date", Date.class);
@@ -40,41 +20,4 @@ class LibraryPropertyTagHandler implements PropertyTagHandler {
         addPropertyToPropertyMap(LibraryProperty.MUSIC_FOLDER, "musicFolder", String.class);
         addPropertyToPropertyMap(LibraryProperty.LIBRARY_PERSISTENT_ID, "libraryPersistentID", String.class);
     }
-
-    private void addPropertyToPropertyMap(String value, String name, Class type) {
-        propertyMap.put(value, name);
-        propertyTypeMap.put(value, type);
-    }
-
-    public void handlePropertyChange(String propertyName) {
-        if (propertyMap.containsKey(propertyName)) {
-            currentProperty = propertyName;
-        } else {
-            currentProperty = NO_PROPERTY;
-            logger.warn("Unsupported Itunes Library Property: " + propertyName);
-        }
-    }
-
-
-    public void handlePropertyValue(Tag propertyValue) {
-        try {
-            if (currentProperty.equals(NO_PROPERTY)) {
-                return;
-            }
-
-            String name = propertyMap.get(currentProperty);
-            if (name != null) {
-                Class type = propertyTypeMap.get(currentProperty);
-                Object value = typeParser.parse(currentProperty, propertyValue, type);
-                new Mirror().on(library).invoke().setterFor(name).withValue(value);
-            } else {
-                logger.warn("Supported Itunes Library Property Was Not Handled Correctly: " + currentProperty);
-            }
-        } catch (Exception e) {
-            logger.error("Error occured during library property parsing: " + e.getMessage(), e);
-        } finally {
-            currentProperty = NO_PROPERTY;
-        }
-    }
-
 }
